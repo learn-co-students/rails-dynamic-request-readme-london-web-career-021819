1dc2a72043753ea45f1648073a6946c4a1803d3b
github username <github email address>
2019-03-15 10:52:37 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :posts, only: :show #refactor. We can use Ruby's RESTful defaults and the resources method
+  # get 'posts/:id', to: 'posts#show'
+
 end
+
+# The /:id tells the routing system that this route can receive a parameter and that the parameter will be passed to the controller's show action.
